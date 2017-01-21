7788970a470bf3e3c7031412f5484d186a6db3ac
videoservices <ps.video.services@gmail.com>
2017-01-21 11:58:55 -0800

    cleanup

--- src/model/selection.js
+++ src/model/selection.js
@@ -2,12 +2,13 @@ const { map, addIndex, times } = require('ramda')
 
 const mapIndexed = addIndex(map)
 const inRange = (a, b, i) => i >= a && i < b
-const selectRange = (a, b, l) => mapIndexed((d, i) => inRange(a, b, i) ? true : d, l)
+
 const ofLength = (n) => times(() => false, n)
+const selectRange = (a, b, l) => mapIndexed((d, i) => inRange(a, b, i) || d, l)
 const sortedBy = (rank, l) => mapIndexed((x, i) => l[rank[i]], l)
 
 module.exports = {
-  selectRange,
   ofLength,
+  selectRange,
   sortedBy
 }
