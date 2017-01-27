de353ea10618d78e305bf8abbe0a6ea22f3ca9e6
Carl Manaster <manaster@pobox.com>
2017-01-27 06:56:36 -0800

    adds Night css theme; toggle between with "d" and "n"

--- src/viz/utils.js
+++ src/viz/utils.js
@@ -1,6 +1,21 @@
+const d3 = require('d3')
+
 const ensureG = (g, className, left, top) => {
   const existingSelection = g.selectAll(`#root_${className}`)
   if (existingSelection._groups[0][0]) return existingSelection
+
+  d3.select('body')
+    .on('keydown', () => {
+      if (d3.event.keyCode === 78) { // Night
+        d3.select('body')
+          .classed('dark', true);
+      }
+      if (d3.event.keyCode === 68) { // Day
+        d3.select('body')
+          .classed('dark', false);
+      }
+    })
+
   return g.append('g')
    .classed(className, true)
    .attr('id', `root_${className}`)
